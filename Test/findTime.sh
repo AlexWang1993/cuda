5aa1a511decf34087913159e0983a6179f013b10
Alex <zh7wang@uwaterloo.ca>
2016-03-08 10:02:14 -0500

    function

--- Test/findTime.sh
+++ Test/findTime.sh
@@ -27,9 +27,9 @@ done
 steps=10
 while [ $steps -lt 2000000 ]; do
     echo "Running, steps = $steps"
-    ../app_debug $price $strike $time $rate $vol 0 0 $digits $steps 0 #>> callBinomialTimeResult_2.log 
+    ../app_debug $price $strike $time $rate $vol 0 1 $digits $steps 0 #>> callBinomialTimeResult_2.log 
     if [ $steps -lt 90000 ]; then
-        ../cpu_app_debug $price $strike $time $rate $vol 0 0 $steps 0 #>> callCpuBinomial_2.log 
+        ../cpu_app_debug $price $strike $time $rate $vol 0 1 $steps 0 #>> callCpuBinomial_2.log 
     fi
     wait
     steps=$((steps*10))
