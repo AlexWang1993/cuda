dc27778ef2e521c20cc16b95ce8190060b4b3dad
Alex <zh7wang@uwaterloo.ca>
2016-03-08 14:42:08 -0500

    function

--- main.cu
+++ main.cu
@@ -66,7 +66,7 @@ computeOptionValue(
     double *w, *w1, *w2;
     int step_limit = THREAD_LIMIT * BLOCK_LIMIT;
     int len = nsteps + 1;
-    int dsize = sizeof(double), 
+    size_t dsize = sizeof(double), 
         size = len * dsize;
 
   //  printf("%d", size);
