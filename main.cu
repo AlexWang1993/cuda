279e39dff525f3e643de5e714b73c8f27f74ad44
Alex <zh7wang@uwaterloo.ca>
2016-03-08 23:33:53 -0500

    function

--- main.cu
+++ main.cu
@@ -109,7 +109,7 @@ computeOptionValue(
         cudaFree(w2);
     } else {
 	//printf("pre-debug2");
-        fprintf(stderr, "space needed: %d\n", TRIANGLE_SIZE_PER_THREAD * size);
+        fprintf(stderr, "space needed: %d\n", THREAD_LIMIT * size);
         cudaMalloc((void **) &w, TRIANGLE_SIZE_PER_THREAD * size);
         checkCudaError("cudaMalloc failed for w.");
 
