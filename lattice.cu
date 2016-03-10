9c6d349b7014dc44b3b176422ad34bf1ab0cee67
Alex <zh7wang@uwaterloo.ca>
2016-03-10 00:50:47 -0500

    function

--- lattice.cu
+++ lattice.cu
@@ -108,7 +108,7 @@ backward_recursion_lower_triangle_multiple(double* w,
 
     for (int i = 0; i < upper; i++) {
         for (int j = 0; j < min(upper - i, n - i - index * upper); j++) {
-            int ind = (i - 1) * len + index * upper + j;
+            int ind = i * len + index * upper + j;
             double res = compute(coef, p, w[ind], w[ind+1], strike, up, down, price, ind, n, type);
             w[ind + len] = res;
         }
@@ -136,7 +136,7 @@ backward_recursion_upper_triangle_multiple(double* w,
     for (int i = 1; i <= upper; i++) {
         int upper_triangle_row_len = upper - i;
         for (int j = 0; j < min(i, n - i - index * upper - upper_triangle_row_len); j++) {
-            int ind = (i - 1) * len + index * upper + upper_triangle_row_len + j;
+            int ind = i * len + index * upper + upper_triangle_row_len + j;
 
             double res = compute(coef, p, w[ind], w[ind+1], strike, up, down, price, ind, n, type);
             if (i == upper) {
