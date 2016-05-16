c8421907379949ccf2865334223f643280a99357
Alex <zh7wang@uwaterloo.ca>
2016-05-15 18:16:06 -0700

    1

--- main.cu
+++ main.cu
@@ -173,8 +173,8 @@ computeOptionValue(
             backward_recursion_upper_triangle_multiple<<<block_num, thread_num>>>(w, i, TRIANGLE_SIZE_PER_THREAD, len, c, prob, strike, up, down, price, type);
             checkCudaError("Failed to compute lower triangles.");
         }
-        cudaMemcpy(answer, w + last_ans_index, dsize, cudaMemcpyDeviceToHost);
-        // cudaMemcpy(answer, w , dsize, cudaMemcpyDeviceToHost);
+        // cudaMemcpy(answer, w + last_ans_index, dsize, cudaMemcpyDeviceToHost);
+        cudaMemcpy(answer, w , dsize, cudaMemcpyDeviceToHost);
         cudaFree(w);
         // answer[0] = answer[0] * pow(c, nsteps);
     }
