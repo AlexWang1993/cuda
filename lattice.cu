b7ea56f1418d6ace53e545191bdbdda44d689377
Alex <zh7wang@uwaterloo.ca>
2016-05-15 18:37:06 -0700

    1

--- lattice.cu
+++ lattice.cu
@@ -220,12 +220,12 @@ backward_recursion_upper_triangle_less_memory(double* w,
               i_left = 3 * len + index - tid + k - 1;
               
             }
-            printf("left: %f ", w[i_left]);
+            // printf("left: %f ", w[i_left]);
             if (tid == upper - 1) {
               i_right = 2 * len + index - tid + upper + k - 1;
               
             }
-            printf("right: %f \n", w[i_right]);
+            // printf("right: %f \n", w[i_right]);
             // printf("level: %d, left: %d, %f, right: %d, %f, self: %d\n", k, i_left, w[i_left], i_right, w[i_right], (n - k) % 2 * len + index);
             double res = compute(coef, p, w[i_left], w[i_right], strike, up, down, price, index, n - k, type);
             w[(n - k) % 2 * len + index] = res;
